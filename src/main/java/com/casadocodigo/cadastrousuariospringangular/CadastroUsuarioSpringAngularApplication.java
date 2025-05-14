b1f4a2acab5fdde14263faa3db5e7d61c2e3963e
brunobarici1981 <brbaricitrin@gmail.com>
2025-05-14 08:01:54 -0300

    Inicio projeto

---
+++ src/main/java/com/casadocodigo/cadastrousuariospringangular/CadastroUsuarioSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.casadocodigo.cadastrousuariospringangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroUsuarioSpringAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroUsuarioSpringAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/casadocodigo/cadastrousuariospringangular/CadastroUsuarioSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casadocodigo.cadastrousuariospringangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsuarioSpringAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
